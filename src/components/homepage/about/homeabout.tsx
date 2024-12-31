16a34a9bf35293b3f358223e6d98d0816532184e
David <lasky96@gmail.com>
2024-12-31 11:40:29 -0500

    added styling

--- src/components/homepage/about/homeabout.tsx
+++ src/components/homepage/about/homeabout.tsx
@@ -2,7 +2,7 @@ import "./homeabout.css"
 
 export default function homeabout() {
   return (
-    <article>
+    <article className="media-flex">
         <h2 className="about-h2">About Our Company</h2>
         <div className="home-about-section">
             <img style={{width:"400px"}} src="\claudio-schwarz-a85IYeAXgxU-unsplash.jpg"/>
@@ -16,17 +16,20 @@ export default function homeabout() {
         </div>
         <div className="double-pic-align">    
             <article>
-                <img src="jan-kopriva-Nvo5xeoccVg-unsplash.jpg"/>
+                <img style={{width:"400px"}} src="\loadedsprinter.jpg"/>
                 <h3>Customer-Centered Approach</h3>
-                <p>Your satisfaction is our priority. We’re committed to tailoring our services
-                     to meet your unique logistics needs.</p>
+                <p>Your satisfaction is our priority. We're committed to tailoring our services
+                     to meet your unique logistics needs. Whether it is a standard pallet
+                     or a complex item we can help you move it.</p>
             </article>
 
             <article>
                 <img src="\norbert-kundrak-shMymEhk86g-unsplash.jpg"/>
                 <h3>A Team You Can Trust</h3>
                 <p style={{paddingBottom:"70px"}}>With a team that is dedicated and has years of experience,
-                    we’re here to make your logistics seamless and stress-free.</p>
+                    we’re here to make your logistics seamless and stress-free. Our team can help you 
+                    save costs on your parcel deliveries.
+                    </p>
             </article>
         </div>
     </article>
